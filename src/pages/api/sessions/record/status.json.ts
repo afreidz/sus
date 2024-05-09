febde2c126c8801a4db1f374e344b8f6189b6d45
Andy Freidenfelds <afreidenfelds@hitachisolutions.com>
2024-05-09 14:16:05 -0500

    fix callback enpoint

--- src/pages/api/sessions/record/status.json.ts
+++ src/pages/api/sessions/record/status.json.ts
@@ -1,6 +1,6 @@
 import { type APIRoute } from "astro";
 
-export const GET: APIRoute = async ({ params, request }) => {
+export const POST: APIRoute = async ({ params, request }) => {
   const body = await request.json();
   console.log(params, body);
 
